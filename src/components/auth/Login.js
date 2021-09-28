6c4ebcd34b60c79d92e49ec988a5cd9d6667a959
ghumbertoenrique <ghumbertoenrique@gmail.com>
2021-09-28 11:52:02 -0400

    First Commit

---
+++ src/components/auth/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return ( 
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesion</h1>
+            </div>
+        </div>
+        );
+}
+ 
+export default Login;
